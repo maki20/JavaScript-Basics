14746cf5eb551790954a08be568519dcc2f8e867
maki20 <makiselecao@gmail.com>
2020-09-20 21:19:21 -0400

    Changed action

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 
+{　
+   document.querySelector('button').addEventListener('click', () => {
+   　　const item1 = document.querySelectorAll('li')[1];
+      
+      //item1.remove();     // 一部の古いブラウザでは使えない。
+
+      //親Node.removeChild();
+      document.querySelector('ul').removeChild(item1);
+
+   });
+}   
+
